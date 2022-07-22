e63e8046c03f292a660101daa7280e8a5180080d
ErrorImCodingkeller <53282401+Error020@users.noreply.github.com>
2022-07-22 19:23:28 +0200

    Update 5(medium).cpp

--- 5(medium).cpp
+++ 5(medium).cpp
@@ -1,6 +1,7 @@
 // This problem was asked by Jane Street.
 
-// cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the firstand last element of that pair.For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+// cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the firstand last element of that pair.For example, 
+// car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
 
 // Given this implementation of cons :
 
